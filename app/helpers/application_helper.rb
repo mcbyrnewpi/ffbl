c2240e9cb030217940567f7ff22d7176496e6813
Matthew Byrne <mcbyrnewpi@yahoo.com>
2015-08-18 16:36:27 -0400

    Create shim and header partial with dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis
+  def full_title(page_title = "")
+    base_title = "Franklin Fantasy Baseball League"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
