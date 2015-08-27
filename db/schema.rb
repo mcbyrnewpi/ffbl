339dbe6c6f973fa22dd0928ecfe34bdd76356793
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2015-08-27 16:02:32 -0400

    Add remember me log in functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818163323) do
+ActiveRecord::Schema.define(version: 20150827181044) do
 
   create_table "levels", force: :cascade do |t|
     t.string   "league"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20150818163323) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
   end
 
   add_index "users", ["team"], name: "index_users_on_team"
