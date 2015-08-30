f6378cfbaaf10c652c38e55734888880c34904ce
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2015-08-30 15:40:41 -0400

    Let admin edit other users information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:new, :create]
+  before_action :admin_user,     only: [:new, :create, :edit, :update]
   
   def index
     @users = User.all.order(:team)
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      redirect_to(root_url) unless current_user?(@user) || current_user.admin?
     end
 
     def admin_user
