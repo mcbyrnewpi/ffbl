4f53686725f92c7fd027c0963583d82dfe0a6e7d
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2015-08-30 13:59:32 -0400

    Add logged_in? as part of checking admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
     end
 
     def admin_user
-      redirect_to(root_url) unless current_user.admin?
+      redirect_to(root_url) unless logged_in? && current_user.admin?
     end
 
 end
