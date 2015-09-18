7e4c263e57bc20d8859e5a1a5b3badd6957a4fae
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2015-09-18 13:23:01 -0400

    Fixed search for case insensitivity in production

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,7 @@ class TransactionsController < ApplicationController
 
 	def index
 		if params[:search]
-			@transactions = Transaction.paginate(:page => params[:page], :per_page => 10).where("team_before LIKE ? OR team_after LIKE ? OR player_last_name LIKE ? OR player_first_name LIKE ?", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%").order("id DESC")
+			@transactions = Transaction.paginate(:page => params[:page], :per_page => 10).where("team_before ILIKE ? OR team_after ILIKE ? OR player_last_name ILIKE ? OR player_first_name ILIKE ?", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%").order("id DESC")
 		else
 			@transactions = Transaction.paginate(:page => params[:page], :per_page => 25).order("id DESC")
 		end
