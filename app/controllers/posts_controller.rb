ba3e1e07424f44348883dc13b98ea9c6da10fa4b
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2015-09-19 11:50:25 -0400

    Create posts controller with index view initiated

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create]
+  before_action :admin_user, only: [:destroy]
+
+  def index
+    @posts = Post.paginate(:page => params[:page], :per_page => 25).order("id DESC")
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Your post has been successfully created!"
+      redirect_to @post
+    else
+      flash[:danger] = "Post could not be created.  Please try again."
+      render 'new'
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    flash[:success] = "Position deleted"
+    redirect_to posts_path
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:topic, :content)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless logged_in? && current_user.admin?
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/destroy'
+
   get 'sessions/new'
 
   root 'static_pages#home'
@@ -28,6 +38,7 @@ Rails.application.routes.draw do
   resources :levels
   resources :players
   resources :transactions
+  resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
