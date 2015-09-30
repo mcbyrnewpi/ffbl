2d3a7b1555b3948e92aa1964d2d66b21086adc92
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2015-09-30 13:45:06 -0400

    Create review contoller with create method

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+	before_action :glctac_user, only: [:create]
+
+	def create
+		@review = Review.new(review_params)
+		if @review.save
+			flash[:success] = "You have posted your comment!"
+			redirect_to @review.book
+		else
+			flash[:danger] = "Could not post review, please try again."
+			redirect_to @review.book
+		end
+	end
+
+	private
+
+		def response_params
+      params.require(:review).permit(:thoughts, :user_id, :book_id)
+    end
+
+		def glctac_user
+      redirect_to(root_url) unless logged_in? && current_user.glctac = true
+    end
+end
