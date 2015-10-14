30c5461800e481680436c7fc20f62a3eeaaf8eff
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2015-10-14 11:34:32 -0400

    Add dependent destroy to players transaction relationship

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ class Player < ActiveRecord::Base
   belongs_to :user
   belongs_to :level
   belongs_to :position
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
 
   validates :last_name,       presence: true
   validates :first_name,      presence: true
