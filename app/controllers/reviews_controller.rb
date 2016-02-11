37535497badea453c049d3795d1603769484514f
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2016-02-11 15:24:21 -0500

    Change book review format to use jQuery to display reviews and append new reviews without reload

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,8 +4,10 @@ class ReviewsController < ApplicationController
 	def create
 		@review = Review.new(review_params)
 		if @review.save
-			flash[:success] = "You have posted your comment!"
-			redirect_to @review.book
+      respond_to do |format|
+        format.js
+        format.html { redirect_to @review.book }
+      end
 		else
 			flash[:danger] = "Could not post review, please try again."
 			redirect_to @review.book
