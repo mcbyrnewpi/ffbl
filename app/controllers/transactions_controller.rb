22e7bea930f8092c8bee8a79bf50057fc72e80e0
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2016-02-24 16:09:45 -0500

    Remove % from player id transaction search

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,7 @@ class TransactionsController < ApplicationController
 
 	def index
 		if params[:search]
-			@transactions = Transaction.paginate(:page => params[:page], :per_page => 25).where("team_before ILIKE ? OR team_after ILIKE ? OR player_last_name ILIKE ? OR player_first_name ILIKE ? OR cast(player_id as text) ILIKE ?", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search])}%").order("id DESC")
+			@transactions = Transaction.paginate(:page => params[:page], :per_page => 25).where("team_before ILIKE ? OR team_after ILIKE ? OR player_last_name ILIKE ? OR player_first_name ILIKE ? OR player_id == ?", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "#{(params[:search])}").order("id DESC")
 		else
 			@transactions = Transaction.paginate(:page => params[:page], :per_page => 25).order("id DESC")
 		end
