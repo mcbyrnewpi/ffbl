ce082626ec35fe7b597bf4b4fba72b41e057013d
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2016-02-24 15:37:52 -0500

    Add player id to transaction search, and add link to transaction history on player show page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,7 @@ class TransactionsController < ApplicationController
 
 	def index
 		if params[:search]
-			@transactions = Transaction.paginate(:page => params[:page], :per_page => 25).where("team_before ILIKE ? OR team_after ILIKE ? OR player_last_name ILIKE ? OR player_first_name ILIKE ?", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%").order("id DESC")
+			@transactions = Transaction.paginate(:page => params[:page], :per_page => 25).where("team_before ILIKE ? OR team_after ILIKE ? OR player_last_name ILIKE ? OR player_first_name ILIKE ? OR player_id ILIKE ?", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search]).strip}%", "%#{(params[:search])}%").order("id DESC")
 		else
 			@transactions = Transaction.paginate(:page => params[:page], :per_page => 25).order("id DESC")
 		end
