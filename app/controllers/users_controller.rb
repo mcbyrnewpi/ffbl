ae1bc42e7422bbd4dcab03f6956d6cdfd057f399
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2016-03-01 17:20:22 -0500

    Add pagination to user show page recent team transactions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @players = @user.players.all.order(:position_id)
-    @transactions = Transaction.where("team_before = ? OR team_after = ?", @user.team, @user.team).order("id DESC")
+    @transactions = Transaction.paginate(:page => params[:page], :per_page => 25).where("team_before = ? OR team_after = ?", @user.team, @user.team).order("id DESC")
     @preseason_report = @user.preseason_reports.last
     @year1 = @players.where("last_name LIKE ?", "%2017")
     @year2 = @players.where("last_name LIKE ?", "%2018")
